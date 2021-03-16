caeeecf38915cfb0ae360ebc5c72737944121a67
Ashish oli <oliashish3@gmail.com>
2021-03-16 13:45:48 +0530

    test commit

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,15 +1,18 @@
 const route = require("express").Router();
-const { Router } = require("express");
-const postcontroller = require("../controller/PostController");
-const Post_Article = postcontroller.Create_Post_Controller;
-const Get_Article = postcontroller.Get_Post_Controller;
 
-route.post("/create", (req, res) => {
+const PostController = require("../controller/postController");
+
+route.get("/", (req, res) => {
+    res.send("welcome to home route of the backend app...");
+    console.log("welcome to home route of the backend app...");
+});
+
+/*route.post("/create", (req, res) => {
     const data = req.body;
-    Post_Article(data);
+    PostController.Post_Controller(data);
 });
 route.get("/getAllArticles", (req, res) => {
-    Get_Article(req, res);
-});
+    PostController.Get_Controller(req, res);
+});*/
 
 module.exports = route;
